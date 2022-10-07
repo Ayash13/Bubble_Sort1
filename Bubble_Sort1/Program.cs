e03d87eb8d5d38b8ec82c0c78d1c5cfbaf5f3d78
Muhammad Ayash <muhammad.ayash.ft21@mail.umy.ac.id>
2022-10-07 17:20:42 +0700

    Method read

--- Bubble_Sort1/Program.cs
+++ Bubble_Sort1/Program.cs
@@ -1,5 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Halo ayash di sini");
-Console.WriteLine("Halo ayash di sini2");
-Console.WriteLine("Halo ayash di sini2");
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bubble_Sort1
+{
+    class Program
+    {
+        //Deklarasi array int dengan ukuran 20 
+        private int[] a = new int[20];
+
+        //Deklarasi variabel int untuk menyimpan banyaknya data apda array
+        private int[] n;
+
+        //Fungsi / Method untuk menerima masukan
+        public void read()
+        {
+            while (true)
+            {
+                Console.WriteLine("Masukan banyaknya elemen pada Array: ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 20)
+                    break;
+                else
+                    Console.WriteLine("Array dapat mempunyai maksimal 20 elemen");
+            }
+            Console.WriteLine("");
+            Console.WriteLine("====================");
+            Console.WriteLine("Masukan elemen array");
+            Console.WriteLine("====================");
+
+            //Pengguna memasukan elemen pada array
+            for (int i = 0; i < n; i++) ;
+            {
+                Console.WriteLine("<" + (i + 1) + ">");
+                string s1 = Console.ReadLine();
+                a[i] = Int32.Parse(s1);
+            }
+        }
+    }
+}
